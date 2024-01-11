fa9f456d0282e536f33ea7be2524a2c1f9e85d7c
飘逸A186 <wlpiaoyi@gmail.com>
2024-01-11 19:36:59 +0800

    提供图片水印工具

--- resource/src/main/java/org/wlpiaoyi/framework/ee/resource/biz/service/impl/file/FileImageHandle.java
+++ resource/src/main/java/org/wlpiaoyi/framework/ee/resource/biz/service/impl/file/FileImageHandle.java
@@ -2,6 +2,8 @@ package org.wlpiaoyi.framework.ee.resource.biz.service.impl.file;
 
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import net.coobird.thumbnailator.Thumbnails;
+import net.coobird.thumbnailator.geometry.Positions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,14 +13,14 @@ import org.wlpiaoyi.framework.ee.resource.biz.service.IImageInfoService;
 import org.wlpiaoyi.framework.utils.MapUtils;
 import org.wlpiaoyi.framework.utils.ValueUtils;
 import org.wlpiaoyi.framework.utils.exception.BusinessException;
+import sun.font.FontDesignMetrics;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.font.FontRenderContext;
+import java.awt.font.LineMetrics;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.OutputStream;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -129,20 +131,112 @@ public class FileImageHandle {
     public static boolean isSupportSuffix(String suffix){
         return imageSuffixeMap.containsKey(suffix);
     }
+
+    /**
+     * 压缩图片
+     * @param orgImagePath
+     * @param suffix
+     * @param smallSize
+     * @param outputStream
+     * @return: void
+     * @author: wlpia
+     * @date: 2024/1/11 18:17
+     */
     @SneakyThrows
-    static void generateSmall(String orgImagePath, String suffix, double smallSize, OutputStream outputStream) {
-        File orgImageFile = new File(orgImagePath);
-        Image orgImage = ImageIO.read(orgImageFile);
-        int width = orgImage.getWidth(null);
-        int height = orgImage.getHeight(null);
-        int widthSmall = (int) (width * smallSize);
-        int heightSmall = (int) (height * smallSize);
-        int imageType = imageSuffixeMap.get(suffix);
-        BufferedImage bi = new BufferedImage(widthSmall, heightSmall, imageType);
-        Graphics g = bi.getGraphics();
-        g.drawImage(orgImage, 0, 0, widthSmall, heightSmall, null);
-        g.dispose();
-        ImageIO.write(bi, suffix, outputStream);
+    public static void generateSmall(String orgImagePath, String suffix, double smallSize, OutputStream outputStream) {
+        Thumbnails.Builder<File> builder = Thumbnails.of(orgImagePath);
+        builder.scale(smallSize)
+                .outputFormat(suffix)   //保存为文件的格式设置
+                .outputQuality(1)    //输出的图片质量  0~1 之间,否则报错
+                .toOutputStream(outputStream);   //输出到指定的输出流中
+    }
+
+    /**
+     * 图片水印
+     * @param orgImagePath  图片路径
+     * @param suffix        图片格式
+     * @param image         水印图片
+     * @param scale         放大缩小水印
+     * @param angle         旋转水印
+     * @param opacity       透明水印
+     * @param outputStream
+     */
+    @SneakyThrows
+    public static void watermark(String orgImagePath, String suffix, BufferedImage image, double scale, double angle, float opacity, OutputStream outputStream){
+
+        Thumbnails.Builder<File> builder = Thumbnails.of(orgImagePath);
+        builder.scale(1.f)
+                .outputFormat(suffix)   //保存为文件的格式设置
+                .outputQuality(1)   //输出的图片质量  0~1 之间,否则报错
+                .watermark(Positions.CENTER,
+                        Thumbnails.of(image).scale(scale).rotate(angle).asBufferedImage(),
+                        opacity)
+                .toOutputStream(outputStream);   //输出到指定的输出流中
+    }
+
+    /**
+     * 文字转字图片
+     * @param text      文本字符串
+     * @param font      设置字体
+     * @param fontColor 字体颜色
+     * @param width     图片宽度
+     * @param height    图片高度
+     * @param alpha     文字透明度，值从0.0f-1.0f，依次变得不透明
+     * @return: java.awt.image.BufferedImage
+     * @author: wlpia
+     * @date: 2024/1/11 18:50
+     */
+    public static BufferedImage parseTextToImage(String text, Font font, Color fontColor, int width, int height, float alpha) {
+       return parseTextToImage(text, font, fontColor, Color.ORANGE, 4, 4, width, height, alpha);
+    }
+    public static BufferedImage parseTextToImage(String text, Font font, Color fontColor, Color shadowColor, int shadowOffsetX, int shadowOffsetY, int width, int height, float alpha) {
+        BufferedImage textImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g2 = textImage.createGraphics();
+        //设置背景透明
+        textImage = g2.getDeviceConfiguration().createCompatibleImage(width, height, Transparency.TRANSLUCENT);
+        g2.dispose();
+        g2 = textImage.createGraphics();
+
+        //开启文字抗锯齿
+        g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        //设置字体
+        g2.setFont(font);
+//        // 创建循环渐变的GraphientPaint对象
+//        GradientPaint paint = new GradientPaint(20, 20, Color.BLUE, 100,120, Color.RED, true);
+//        g2.setPaint(paint);// 设置渐变
+        //设置透明度:1.0f为透明度 ，值从0-1.0，依次变得不透明
+        g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha));
+        //计算字体位置：上下左右居中
+        FontRenderContext context = g2.getFontRenderContext();
+        LineMetrics lineMetrics = font.getLineMetrics(text, context);
+        FontMetrics fontMetrics = FontDesignMetrics.getMetrics(font);
+        float offset = (width - fontMetrics.stringWidth(text)) / 2;
+        float y = (height + lineMetrics.getAscent() - lineMetrics.getDescent() - lineMetrics.getLeading()) / 2;
+        //绘图
+        g2.setColor(shadowColor);
+        g2.drawString(text, (int) offset + shadowOffsetX, (int) y + shadowOffsetY);
+        //设置字体颜色
+        g2.setColor(fontColor);
+        g2.drawString(text, (int) offset, (int) y);
+        //释放资源
+        g2.dispose();
+        return textImage;
+    }
+
+    @SneakyThrows
+    public static void main(String[] args) {
+//        generateSmall("D:\\wlpia\\Documents\\Temp\\微信图片_20231227174448.jpg", "jpg", 0.3f,
+//                new FileOutputStream(new File("D:\\wlpia\\Documents\\Temp\\1.jpg")));
+        BufferedImage bufferedImage = parseTextToImage("请在这里输入文字",
+                        new Font("微软雅黑", Font.BOLD, 100),
+                        Color.BLACK, 1000, 200, 1.0f);
+        watermark("D:\\wlpia\\Documents\\Temp\\微信图片_20231227174448.jpg",
+                "jpg", bufferedImage, 4., 45., 0.5f,
+                new FileOutputStream(new File("D:\\wlpia\\Documents\\Temp\\1.jpg")));
+        ImageIO.write(bufferedImage, "png",
+                        new FileOutputStream(new File("D:\\wlpia\\Documents\\Temp\\1.png")));
+
+
     }
 
 }
